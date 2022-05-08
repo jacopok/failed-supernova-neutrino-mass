1d4f4fb9c3b330ba53c0d5afca49612c5ad36e1c
jacopok <jacopok@gmail.com>
2022-05-08 22:25:34 +0200

    adapted QNM estimates

--- QNM_estimates.py
+++ QNM_estimates.py
@@ -4,11 +4,12 @@ import numpy as np
 import astropy.units as u
 import astropy.constants as ac
 
-M = 60 * u.Msun
+M = 3 * u.Msun
 
-frequency = (.747343 * ac.c ** 3 / (4 * np.pi * ac.G * M)).si
+frequency = (.747343 * ac.c ** 3 / (4 * np.pi * ac.G * M)).to(u.kHz)
 
-gamma = (.177925 * ac.c**3 / (2 * ac.G * M)).si
+gamma = (1/(.177925 * ac.c**3 / (2 * ac.G * M))).to(u.ms)
 
-print(frequency)
-print(gamma)
+print(f'Mass: {M}')
+print(f'Lowest QNM frequency: {frequency}')
+print(f'Corresponding decay timescale: {gamma}')
