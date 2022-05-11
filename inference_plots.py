ff0811fad71a49ed42191af0b24da56b32bf2e1f
jacopok <jacopok@gmail.com>
2022-05-11 23:17:15 +0200

    new plot!

--- inference_plots.py
+++ inference_plots.py
@@ -8,7 +8,7 @@ from inference import reconstructed_mass
 
 masses = np.linspace(1, 120, num=20) * u.meV
 
-n_trials = 20
+n_trials = 100
 
 reconstructed_masses = np.repeat(np.copy(masses)[:, None], n_trials, axis=1)
 
@@ -16,8 +16,13 @@ for i, mass in tqdm(enumerate(masses)):
     for j in range(n_trials):
         reconstructed_masses[i, j] = reconstructed_mass(mass, 50 * u.kpc)
 
+probabilities = np.zeros_like(masses.value)
+
 for j in range(n_trials):
     plt.scatter(masses, reconstructed_masses[:, j], s=1, c='black')
+
+for i in range(len(masses)):
+    probabilities[i] = sum(reconstructed_masses[i, :] > 1e-3 * u.meV) / n_trials
 
 plt.plot(masses, masses)
 
@@ -25,3 +30,5 @@ plt.xlabel('Original mass [meV]')
 plt.ylabel('Reconstructed mass [meV]')
 plt.show()
 
+plt.plot(masses, probabilities)
+plt.show()
