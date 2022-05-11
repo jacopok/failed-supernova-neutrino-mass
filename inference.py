05668df6963fdcb38178350bbd4516c2c61eca11
jacopok <jacopok@gmail.com>
2022-05-11 18:40:03 +0200

    refactored using rate

--- inference.py
+++ inference.py
@@ -7,8 +7,7 @@ from tqdm import tqdm
 from delays import delay
 from flux_estimate import energies, energy_generator
 
-MAX_TIME = 10. # maximum time for the arrival, seconds
-EVENT_RATE = 4e3 # events per second
+MAX_TIME = .5 # maximum time for the arrival, seconds
 
 @dataclass
 class DetectedNeutrino:
@@ -18,21 +17,21 @@ class DetectedNeutrino:
     def apply_delay(self, neutrino_mass: u.Quantity[u.meV], distance: u.Quantity[u.kpc]):
         self.arrival_time += delay(neutrino_mass, self.energy * u.MeV, distance).to(u.s).value
 
-    def estimated_mass(self, distance: u.Quantity[u.kpc]):
-        delta_t = self.arrival_time - MAX_TIME
+    def estimated_mass(self, distance: u.Quantity[u.kpc], max_time: float = MAX_TIME):
+        delta_t = self.arrival_time - max_time
         if delta_t < 0:
             return 0 * u.meV
         
         return (self.energy * u.MeV * np.sqrt(delta_t * u.s * ac.c / distance)).to(u.meV)
 
 
-
-
-def make_neutrino_list(rate: float = EVENT_RATE, max_time: float = MAX_TIME):
+def make_neutrino_list(max_time: float = MAX_TIME):
     
     time = 0
     
     rng = np.random.default_rng()
+    
+    avg_waiting_time = (1/energy_generator.rate).to(u.s).value
     
     neutrinos = []
     
@@ -44,40 +43,24 @@ def make_neutrino_list(rate: float = EVENT_RATE, max_time: float = MAX_TIME):
             )
         )
         
-        waiting_time = rng.exponential(scale = 1/rate)
+        waiting_time = rng.exponential(scale = avg_waiting_time)
         time += waiting_time
     
     return neutrinos
 
 def reconstructed_mass(mass: u.Quantity[u.meV], distance: u.Quantity[u.kpc]):
-    neutrinos = make_neutrino_list()
+    max_time = 0.01
+    neutrinos = make_neutrino_list(max_time=max_time)
     for n in neutrinos:
         n.apply_delay(mass, distance)
 
     max_delay_event = max(neutrinos, key=lambda n: n.arrival_time)
 
-    return max_delay_event.estimated_mass(distance)
+    return max_delay_event.estimated_mass(distance, max_time=max_time)
+
+
 
 if __name__ == '__main__':
-    
-    import matplotlib.pyplot as plt
-
-    masses = np.linspace(1, 120, num=20) * u.meV
-    
-    n_trials = 20
-
-    # reconstructed_masses = np.repeat(np.copy(masses)[:, None], n_trials, axis=1)
-    
-    # for i, mass in tqdm(enumerate(masses)):
-    #     for j in range(n_trials):
-    #         reconstructed_masses[i, j] = reconstructed_mass(mass, 50 * u.kpc)
-    
-    # for j in range(n_trials):
-    #     plt.scatter(masses, reconstructed_masses[:, j], s=1, c='black')
-
-
-    # plt.xlabel('Original mass [meV]')
-    # plt.ylabel('Reconstructed mass [meV]')
 
     neutrinos = make_neutrino_list()
     for n in neutrinos:
